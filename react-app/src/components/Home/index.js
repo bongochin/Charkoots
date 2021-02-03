bdfd1f6737caba12b93b461a686b6437cda00cd7
Dan Chin <danielmchin@outlook.com>
2021-02-03 18:04:58 -0500

    resolve merge conflict

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -20,7 +20,7 @@ function Home() {
         It's your perfect romantic weekend getaway, and you really want to make it special this time. Candles, check. Luther Vandross, check. You nailed it all but there's one problem; what about food and wine?
         Let us take care of that for you. Charkoots is a place where you can create and customize a delicious charcuterie board with our finest selection of cured meats,
         artisanal cheeses, crackers and more to keep your cravings satisfied. Not only that, we'll take a deeper dive into what you have created, and pair you with the PERFECT wine to complement the experience.
-        Too good to be true? I know, and you're welcome.
+        Too good to be true? I know, and you're welcome!
       </p>
       <p>Click on the button below to start building your very own Charkoot board!</p>
       <button>START YOUR ORDER</button>
