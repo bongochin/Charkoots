1de026e9bbcae905b39d1baae8002aa1c56a1626
Dan Chin <danielmchin@outlook.com>
2021-02-04 22:46:36 -0500

    favicon, logo, navbar styling

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
+import logo from './logo.png'
 import LogoutButton from '../auth/LogoutButton';
 import './NavBar.css'
 
 const NavBar = ({ setAuthenticated }) => {
+  const history = useHistory()
+
   return (
     <nav className="navbar-wrapper">
         <div className="navbar-home">
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
+          <img
+            className="navbar-home_logo"
+            onClick={() => history.push('/')}
+            src={logo}
+            alt=""
+          />
         </div>
         <div className="navbar-tabs">
           <NavLink to="/orders" exact={true} activeClassName="active">
             Start Your Order
           </NavLink>
+          <br/>
           <NavLink to="/menus" exact={true} activeClassName="active">
             Menus
           </NavLink>
