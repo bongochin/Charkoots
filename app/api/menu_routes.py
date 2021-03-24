0f60fafe5d2efc018a14cdb3b25edb3fe6171dff
Dan Chin <danielmchin@outlook.com>
2021-03-24 12:25:34 -0400

    menu routes

--- app/api/menu_routes.py
+++ app/api/menu_routes.py
@@ -19,3 +19,28 @@ def boardItems():
 def meats():
   get_meats = Board_Item1.query.all()
   return jsonify(([meat.to_dict() for meat in get_meats]))
+
+@menu_routes.route('/cheeses')
+def cheeses():
+  get_cheeses = Board_Item4.query.all()
+  return jsonify(([cheese.to_dict() for cheese in get_cheeses]))
+
+@menu_routes.route('/crackers')
+def crackers():
+  get_crackers = Board_Item7.query.all()
+  return jsonify(([cracker.to_dict() for cracker in get_crackers]))
+
+@menu_routes.route('/fruits')
+def fruits():
+  get_fruits = Board_Item8.query.all()
+  return jsonify(([fruit.to_dict() for fruit in get_fruits]))
+
+@menu_routes.route('/nuts')
+def nuts():
+  get_nuts = Board_Item9.query.all()
+  return jsonify(([nut.to_dict() for nut in get_nuts]))
+
+@menu_routes.route('/spreads')
+def spreads():
+  get_spreads = Board_Item10.query.all()
+  return jsonify(([spread.to_dict() for spread in get_spreads]))
